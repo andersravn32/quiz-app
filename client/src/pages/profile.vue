f10ae2037bd918e069ebc8b29eeae5c3f6632a7b
Anders Ravn <anders@adriansen.dk>
2023-03-13 16:08:30 +0100

    Scaffold: Scaffolded profile page

--- client/src/pages/profile.vue
+++ client/src/pages/profile.vue
@@ -1,3 +1,15 @@
 <template>
-    <div></div>
+  <section id="page-profile" class="container">
+    <h1>Min profil</h1>
+
+    <hgroup>
+      <h2>Generel info</h2>
+      <h3><small>Her kan du ændre generel information vedrørende din konto</small></h3>
+    </hgroup>
+
+    <hgroup>
+      <h2>API info</h2>
+      <h3><small>Her finder du information vedrørende dine API nøgler</small></h3>
+    </hgroup>
+  </section>
 </template>
