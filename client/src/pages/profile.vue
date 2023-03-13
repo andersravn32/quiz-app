da590b819718c5356fdfe9b609a8249204fd9054
Anders Ravn <anders@adriansen.dk>
2023-03-13 18:10:33 +0100

    Add: Added profile edit form to profile pge

--- client/src/pages/profile.vue
+++ client/src/pages/profile.vue
@@ -1,15 +1,24 @@
+<script setup>
+import FormProfileEdit from '../components/FormProfileEdit.vue';
+</script>
+
 <template>
   <section id="page-profile" class="container">
     <h1>Min profil</h1>
 
     <hgroup>
       <h2>Generel info</h2>
-      <h3><small>Her kan du ændre generel information vedrørende din konto</small></h3>
+      <h3>
+        <small>Her kan du ændre generel information vedrørende din konto</small>
+      </h3>
     </hgroup>
+    <FormProfileEdit />
 
     <hgroup>
       <h2>API info</h2>
-      <h3><small>Her finder du information vedrørende dine API nøgler</small></h3>
+      <h3>
+        <small>Her finder du information vedrørende dine API nøgler</small>
+      </h3>
     </hgroup>
   </section>
 </template>
