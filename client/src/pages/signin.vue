07c59a18da8f34d2399d36ba5731f0abf799e11f
Anders Ravn <anders@adriansen.dk>
2023-03-13 15:27:16 +0100

    Add: Added signin page

--- client/src/pages/signin.vue
+++ client/src/pages/signin.vue
@@ -1,3 +1,72 @@
+<script setup>
+import { ref } from "vue";
+import { useRouter } from "vue-router";
+import useAccount from "../composables/useAccount";
+
+const router = useRouter();
+
+const account = useAccount();
+
+const loading = ref(false);
+
+const user = ref({
+  email: null,
+  password: null,
+});
+
+const signin = async () => {
+  // Update loading state
+  loading.value = true;
+
+  // Request signup
+  const request = await fetch("http://127.0.0.1:3000/auth/signin", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(user.value),
+  }).then((res) => res.json());
+
+  // Update loading state
+  loading.value = false;
+
+  // If signup wasnt successful
+  if (!request.accessToken) {
+    return;
+  }
+
+  localStorage.setItem("refreshToken", request.refreshToken);
+  localStorage.setItem("accessToken", request.accessToken);
+
+  account.value = request.user;
+
+  return router.push("/dashboard");
+};
+</script>
+
 <template>
-    <div></div>
+  <section id="page-signin" @submit.prevent="signin">
+    <form class="container">
+      <h1>Log på</h1>
+      <label for="email">
+        Email
+        <input v-model="user.email" type="email" placeholder="Email" required />
+      </label>
+      <label for="password">
+        Password
+        <input
+          v-model="user.password"
+          type="password"
+          placeholder="Password"
+          required
+        />
+      </label>
+      <button :aria-busy="loading">
+        {{ loading ? "Vent venligst" : "Opret konto" }}
+      </button>
+      <p>
+        Har du ingen konto? <router-link to="/signup">Opret mig</router-link>
+      </p>
+    </form>
+  </section>
 </template>
