107c657f15565559c036c18841929bcb710e04bd
Anders Ravn <anders@adriansen.dk>
2023-03-13 16:48:50 +0100

    Add: Added category router

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const router = express.Router();
+const check = require("../middleware/check")
 
 // Primary auth route
 router.use("/auth", require("./auth"));
 
-
+// Primary category route
+router.use("/category", check.auth, require("./category"))
 module.exports = router;
