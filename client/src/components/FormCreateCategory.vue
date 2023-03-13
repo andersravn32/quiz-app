9f695aa5be1be2bb00156b9fe57ea294a0fe90ac
Anders Ravn <anders@adriansen.dk>
2023-03-13 16:08:52 +0100

    Add: Added admin forms

---
+++ client/src/components/FormCreateCategory.vue
@@ -0,0 +1,71 @@
+<script setup>
+import { ref } from "vue";
+
+const emit = defineEmits(["close"]);
+
+const category = ref({
+  title: null,
+  description: null,
+  public: true,
+});
+
+const loading = ref(false);
+
+const create = async () => {
+    // Update loading state
+    loading.value = true;
+
+    
+
+    // Update loading state
+    loading.value = false;
+};
+</script>
+
+<template>
+  <header>
+    <a
+      href="#close"
+      aria-label="Close"
+      class="close"
+      @click="emit('close')"
+    ></a>
+    Opret ny kategori
+  </header>
+  <form id="form-create-category" @submit.prevent="create">
+    <label for="email">
+      Titel på kategori
+      <input
+        v-model="category.title"
+        type="text"
+        placeholder="Titel"
+        required
+      />
+    </label>
+    <label for="email">Kategori beskrivelse</label>
+    <textarea
+      v-model="category.description"
+      type="email"
+      placeholder="Beskrivelse"
+      required
+    ></textarea>
+    <label>
+      <input v-model="category.public" type="checkbox" role="switch" />
+      Offentliggør kategori
+    </label>
+    <button :aria-busy="loading">
+      {{ loading ? "Vent venligst" : "Opret kategori" }}
+    </button>
+  </form>
+</template>
+
+<style>
+#form-create-category button {
+  margin-top: 2rem;
+}
+
+#form-create-category textarea {
+  resize: none;
+  min-height: 125px;
+}
+</style>
--- client/src/components/Navbar.vue
+++ client/src/components/Navbar.vue
@@ -1,14 +1,23 @@
 <script setup>
-import useAccount from "../composables/useAccount";
+import FormCreateCategory from "./FormCreateCategory.vue";
+import FormCreateQuiz from "./FormCreateQuiz.vue";
 
+import { useRouter } from "vue-router";
+import useAccount from "../composables/useAccount";
+import useModal from "../composables/useModal";
+
+const router = useRouter();
 const account = useAccount();
+const modal = useModal();
+
 </script>
 
 <template>
   <nav id="navbar" class="container">
     <ul>
       <li>
-        <strong>Quiz app</strong>
+        <strong v-if="account" @click="router.push('/dashboard')">Quiz app</strong>
+        <strong v-if="!account">Quiz app</strong>
       </li>
     </ul>
     <ul v-if="account">
@@ -19,9 +28,9 @@ const account = useAccount();
         <details role="list" dir="rtl">
           <summary aria-haspopup="listbox" role="link">Administrator</summary>
           <ul role="listbox">
-            <li><a>Opret ny quiz</a></li>
-            <li><a>Opret ny kategori</a></li>
-            <li><a>Administrer brugere</a></li>
+            <li><a href="#" @click="modal.setComponent(FormCreateQuiz); modal.toggle()">Opret ny quiz</a></li>
+            <li><a href="#" @click="modal.setComponent(FormCreateCategory); modal.toggle()">Opret ny kategori</a></li>
+            <li><a href="#">Administrer brugere</a></li>
           </ul>
         </details>
       </li>
