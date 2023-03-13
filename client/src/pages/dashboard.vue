87e6893f88ecad80a1e9281c099cf7c0eac72767
Anders Ravn <anders@adriansen.dk>
2023-03-13 16:08:40 +0100

    Scaffold: Scaffolded dashboard

--- client/src/pages/dashboard.vue
+++ client/src/pages/dashboard.vue
@@ -1,9 +1,20 @@
 <script setup>
-import useAccount from '../composables/useAccount';
+import useAccount from "../composables/useAccount";
 
-const account = useAccount()
+const account = useAccount();
 </script>
 
 <template>
-    <div></div>
+  <section id="page-dashboard" class="container">
+    <h1>Dashboard</h1>
+    <hgroup>
+      <h2>Velkommen {{ account.firstName }}!</h2>
+      <h3>
+        <small>Nedenfor kan du se de quizzer som du kan tage.</small>
+      </h3>
+    </hgroup>
+    <div class="grid">
+        
+    </div>
+  </section>
 </template>
