160bf13d128e59b8db59e30c4e4ca5b79e43ca2e
Anders Ravn <anders@adriansen.dk>
2023-03-14 11:06:27 +0100

    Add: Added dashboard grid

--- client/src/pages/dashboard.vue
+++ client/src/pages/dashboard.vue
@@ -1,11 +1,20 @@
 <script setup>
+import { onMounted } from "vue";
 import useAccount from "../composables/useAccount";
+import { useRouter } from "vue-router";
 
+const router = useRouter();
 const account = useAccount();
+
+onMounted(() => {
+  if (router.currentRoute.value.meta.auth && !account.value) {
+    return router.push("/signin");
+  }
+});
 </script>
 
 <template>
-  <section id="page-dashboard" class="container">
+  <section v-if="account" id="page-dashboard" class="container">
     <h1>Dashboard</h1>
     <hgroup>
       <h2>Velkommen {{ account.firstName }}!</h2>
@@ -13,8 +22,6 @@ const account = useAccount();
         <small>Nedenfor kan du se de quizzer som du kan tage.</small>
       </h3>
     </hgroup>
-    <div class="grid">
-        
-    </div>
+    <div class="grid"></div>
   </section>
 </template>
