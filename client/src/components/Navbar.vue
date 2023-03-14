821c6590a5691938ad98f094ac30a3ea0cbaae23
Anders Ravn <anders@adriansen.dk>
2023-03-14 11:05:59 +0100

    Remove: Removed quiz creation from navbar

--- client/src/components/Navbar.vue
+++ client/src/components/Navbar.vue
@@ -1,6 +1,5 @@
 <script setup>
 import FormCreateCategory from "./FormCreateCategory.vue";
-import FormCreateQuiz from "./FormCreateQuiz.vue";
 
 import { useRouter } from "vue-router";
 import useAccount from "../composables/useAccount";
@@ -9,14 +8,15 @@ import useModal from "../composables/useModal";
 const router = useRouter();
 const account = useAccount();
 const modal = useModal();
-
 </script>
 
 <template>
   <nav id="navbar" class="container">
     <ul>
       <li>
-        <strong v-if="account" @click="router.push('/dashboard')">Quiz app</strong>
+        <strong v-if="account" @click="router.push('/dashboard')"
+          >Quiz app</strong
+        >
         <strong v-if="!account">Quiz app</strong>
       </li>
     </ul>
@@ -28,8 +28,21 @@ const modal = useModal();
         <details role="list" dir="rtl">
           <summary aria-haspopup="listbox" role="link">Administrator</summary>
           <ul role="listbox">
-            <li><a href="#" @click="modal.setComponent(FormCreateQuiz); modal.toggle()">Opret ny quiz</a></li>
-            <li><a href="#" @click="modal.setComponent(FormCreateCategory); modal.toggle()">Opret ny kategori</a></li>
+            <li>
+              <router-link to="/quiz/create"
+                >Opret ny quiz</router-link
+              >
+            </li>
+            <li>
+              <a
+                href="#"
+                @click="
+                  modal.setComponent(FormCreateCategory);
+                  modal.toggle();
+                "
+                >Opret ny kategori</a
+              >
+            </li>
             <li><a href="#">Administrer brugere</a></li>
           </ul>
         </details>
@@ -48,5 +61,6 @@ const modal = useModal();
   top: 0;
   left: 0;
   right: 0;
+  background-color:#11191f;
 }
 </style>
