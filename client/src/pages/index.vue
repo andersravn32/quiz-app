225a4b88c5f1f0202c5b76636a541cde2f83f2bc
Anders Ravn <anders@adriansen.dk>
2023-03-14 16:17:12 +0100

    Add: added index page

--- client/src/pages/index.vue
+++ client/src/pages/index.vue
@@ -1,3 +1,22 @@
 <template>
-    <div></div>
+  <section id="page-index" class="container">
+    <div class="inner">
+      <hgroup>
+        <h1>Velkommen til min quiz app</h1>
+        <h2>Opret venligst en bruger</h2>
+      </hgroup>
+      <img src="https://media.tenor.com/BvHv9P7CnBMAAAAC/no-sully.gif" alt="Sully">
+    </div>
+  </section>
 </template>
+
+<style>
+#page-index .inner {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  text-align:center;
+  padding:4rem;
+}
+</style>
