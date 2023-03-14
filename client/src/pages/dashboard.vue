cd85ad63f5764f61336472d6c5ccf62e16b31c90
Anders Ravn <anders@adriansen.dk>
2023-03-14 14:13:31 +0100

    Add: Added dashboard

--- client/src/pages/dashboard.vue
+++ client/src/pages/dashboard.vue
@@ -1,10 +1,36 @@
 <script setup>
-import { onMounted } from "vue";
+import { onMounted, ref } from "vue";
 import useAccount from "../composables/useAccount";
 import { useRouter } from "vue-router";
+import useData from "../composables/useData";
 
 const router = useRouter();
 const account = useAccount();
+const data = useData();
+
+// Update data in composable
+if (!data.quizzes.value.length || !data.categories.value.length) {
+  await data.refresh();
+}
+
+data.quizzes.value.forEach(async (quiz, index) => {
+  if (quiz.creatorFull){
+    return;
+  }
+  
+  const creatorFull = await fetch(
+    `http://127.0.0.1:3000/profile/${quiz.creator}`,
+    {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: localStorage.getItem("accessToken"),
+      },
+    }
+  ).then((res) => res.json());
+
+  data.quizzes.value[index].creatorFull = creatorFull.data;
+});
 
 onMounted(() => {
   if (router.currentRoute.value.meta.auth && !account.value) {
@@ -22,6 +48,77 @@ onMounted(() => {
         <small>Nedenfor kan du se de quizzer som du kan tage.</small>
       </h3>
     </hgroup>
-    <div class="grid"></div>
+    <div class="quizzes">
+      <article
+        v-for="(quiz, index) in data.quizzes.value"
+        :key="index"
+        class="quiz"
+      >
+        <header>{{ quiz.title }}</header>
+        <div class="body">
+          <p>
+            Kategori:
+            <span
+              :data-tooltip="
+                data.categories.value.filter((cat) => {
+                  return cat._id == quiz.category;
+                })[0].description
+              "
+              >{{
+                data.categories.value.filter((cat) => {
+                  return cat._id == quiz.category;
+                })[0].title
+              }}</span
+            >
+          </p>
+          <p>Antal spørgsmål: {{ quiz.questions.length }}</p>
+        </div>
+        <button @click="router.push(`/quiz/${quiz._id}`)">Start quiz</button>
+        <footer>
+          <small>Oprettet af: </small>
+          <div v-if="quiz.creatorFull" class="user">
+            <span
+              >{{ quiz.creatorFull.firstName }}
+              {{ quiz.creatorFull.lastName }}</span
+            >
+            <span>@{{ quiz.creatorFull.identifier }}</span>
+          </div>
+        </footer>
+      </article>
+    </div>
   </section>
 </template>
+
+<style>
+.quizzes {
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  gap: 1rem;
+}
+
+.quiz .body {
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+}
+
+.quiz footer {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+
+.user {
+  display: flex;
+  flex-direction: column;
+  text-align: right;
+}
+
+.user span:nth-of-type(1) {
+  font-size: 0.875em;
+}
+
+.user span:nth-of-type(2) {
+  opacity: 50%;
+  font-size: 0.75em;
+}
+</style>
