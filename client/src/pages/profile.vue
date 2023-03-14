23a4600d2ad80602ad3b3f55d2818d3c63a33701
Anders Ravn <anders@adriansen.dk>
2023-03-14 16:18:40 +0100

    Add: Added sync method to profile loading

--- client/src/pages/profile.vue
+++ client/src/pages/profile.vue
@@ -3,13 +3,39 @@ import FormProfileEdit from "../components/FormProfileEdit.vue";
 import useAccount from "../composables/useAccount";
 import { useRouter } from "vue-router";
 import { onMounted } from "vue";
+import useData from "../composables/useData";
 
 const router = useRouter();
 const account = useAccount();
+const data = useData();
 
-onMounted(() => {
+data.answers.value.forEach((answer, index) => {
+  // Match quiz to answer
+  const quiz = data.quizzes.value.filter((quiz) => {
+    return quiz._id == answer.quiz;
+  })[0];
+
+  // Get amount of correct answers
+  let correct = 0;
+  answer.answers.forEach((ans, index) => {
+    if (quiz.questions[index].options[ans.answer].correct) {
+      correct++;
+    }
+  });
+
+  // Set new correct percentage value on individual answers in composable
+  data.answers.value[index].correctPerc = Math.round(
+    (correct / quiz.questions.length) * 100
+  );
+});
+
+onMounted(async () => {
   if (router.currentRoute.value.meta.auth && !account.value) {
     return router.push("/signin");
+  }
+
+  if (data.requestRefresh.value) {
+    await data.refresh();
   }
 });
 </script>
@@ -25,7 +51,53 @@ onMounted(() => {
       </h3>
     </hgroup>
     <FormProfileEdit />
-
+    <hgroup>
+      <h2>Statistik</h2>
+      <h3>
+        <small
+          >Her kan du se generel statistik vedrørende de forskellige quizzes du
+          har gennemført</small
+        >
+      </h3>
+    </hgroup>
+    <table v-if="data.answers.value.length" role="grid">
+      <thead>
+        <tr>
+          <th scope="col">Quiz ID</th>
+          <th scope="col">Quiz Titel</th>
+          <th scope="col">Kategori</th>
+          <th scope="col">Dato</th>
+          <th scope="col">Rigtige</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="answer in data.answers.value">
+          <th scope="row">{{ answer.quiz }}</th>
+          <td>
+            {{
+              data.quizzes.value.filter((quiz) => {
+                return quiz._id == answer.quiz;
+              })[0].title
+            }}
+          </td>
+          <td>
+            {{
+              data.categories.value.filter((category) => {
+                return (
+                  category._id ==
+                  data.quizzes.value.filter((quiz) => {
+                    return quiz._id == answer.quiz;
+                  })[0].category
+                );
+              })[0].title
+            }}
+          </td>
+          <td>{{ new Date(answer.created).toISOString().split("T")[0] }}</td>
+          <td>{{ answer.correctPerc }} %</td>
+        </tr>
+      </tbody>
+    </table>
+    <p v-if="!data.answers.value.length">Fandt ingen besvarelser...</p>
     <hgroup>
       <h2>API info</h2>
       <h3>
